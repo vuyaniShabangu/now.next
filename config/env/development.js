e648abe5b878d95df378ea5dcad4abf2885a4cba
Vuyani M. Shabangu <vuyani.shabangu@gmail.com>
2016-06-09 18:04:58 +0200

    Updating dev configurations to MongoDB

--- config/env/development.js
+++ config/env/development.js
@@ -4,10 +4,10 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'ds011374.mlab.com:11374/heroku_9s70v4j8') + '/mean-dev',
     options: {
-      user: '',
-      pass: ''
+      user: 'root',
+      pass: 'root'
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
