c75ae163012ea016f2dcf9f6fc8ea85077cb09a3
msthosago09 <u13062060@tuks.co.za>
2016-06-27 16:03:12 +0200

    Trying to read drones from database

--- controllers/manage-drones.js
+++ controllers/manage-drones.js
@@ -1,5 +1,33 @@
-exports.getMyDrones = (req, res) => {
-  res.render('manage-drones', {
-    title: 'Manage Drones'
-  });
+const nodemailer = require('nodemailer');
+const Drone = require('../models/Drone');
+const User = require('../models/User');
+var tmpEmail = User.email;
+var listDrones;
+var mongoos = require('mongoose'), DroneRead = mongoose.model('Drone');
+
+module.exports = {
+	all:function(req,res){
+		DroneRead.find({},function(err,docs){
+			if(err) res.send(err);
+			res.json(docs);
+		})
+	}
 };
+
+/*
+Drone.find().where('fUser').eq(tmpEmail).exec(function(err,drns){
+	listDrones = drns;
+});
+*/
+exports.getMyDrones = (req, res) => {
+	res.render('manage-drones', {title: 'Manage Drones'},{droneList:listDrones});
+};
+/*
+exports.getMyDrones = (req, res) => {
+	Drone.find((err,docs)=> {
+	res.render('manage-drones', {
+	droneList:docs
+	});
+});
+};
+*/
