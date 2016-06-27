775190da541eb14d9a853a96bf5dfe54f9b86cfb
MahobalaThosago <u13062060@tuks.co.za>
2016-06-27 16:24:19 +0200

    Update manage-drones.js

--- controllers/manage-drones.js
+++ controllers/manage-drones.js
@@ -3,26 +3,16 @@ const Drone = require('../models/Drone');
 const User = require('../models/User');
 var tmpEmail = User.email;
 var listDrones;
-var mongoose = require('mongoose'), DroneRead = mongoose.model('Drone');
-
-module.exports = {
-	all:function(req,res){
-		DroneRead.find({},function(err,docs){
-			if(err) res.send(err);
-			res.json(docs);
-		})
-	}
-};
 
 /*
 Drone.find().where('fUser').eq(tmpEmail).exec(function(err,drns){
 	listDrones = drns;
 });
-*/
+
 exports.getMyDrones = (req, res) => {
 	res.render('manage-drones', {title: 'Manage Drones'},{droneList:listDrones});
 };
-/*
+*/
 exports.getMyDrones = (req, res) => {
 	Drone.find((err,docs)=> {
 	res.render('manage-drones', {
@@ -30,4 +20,4 @@ exports.getMyDrones = (req, res) => {
 	});
 });
 };
-*/
+
