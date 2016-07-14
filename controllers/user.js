dffd85276a08b6462a70c968e3d0f9376f572b40
Sibusiso Masemola <sbulink@users.noreply.github.com>
2016-07-14 11:24:23 +0200

    Update User.js
    1.1

--- controllers/user.js
+++ controllers/user.js
@@ -4,11 +4,6 @@ const nodemailer = require('nodemailer');
 const passport = require('passport');
 const User = require('../models/User');
 
-
-
-
-
-
 /**
  * GET /login
  * Login page.
@@ -99,14 +94,13 @@ exports.postSignup = (req, res, next) => {
 
   const user = new User({
     email: req.body.email,
-    password: req.body.password 
+    password: req.body.password,
+    name: req.body.name,
+    surname: req.body.surname,
+    username: req.body.username,
+    phonenumber: req.body.phonenumber,
+    role: req.body.role 
   });
-
-  user.profile.name= req.body.name;
-    user.profile.surname= req.body.surname;
-    user.profile.username= req.body.username;
-    user.profile.phonenumber= req.body.phonenumber;
-    user.profile.role= req.body.role;
 
   User.findOne({ email: req.body.email }, (err, existingUser) => {
     if (existingUser) {
