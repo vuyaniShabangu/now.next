de0c7199feaa2a0418ca60e241a69fbee5016f46
Sibusiso Masemola <sbulink@users.noreply.github.com>
2016-07-14 11:26:35 +0200

    Update app.js
    1.1 Lusca added.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const errorHandler = require('errorhandler');
-//const lusca = require('lusca');
+const lusca = require('lusca');
 const dotenv = require('dotenv');
 const MongoStore = require('connect-mongo')(session);
 const flash = require('express-flash');
@@ -82,7 +82,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
-/*app.use((req, res, next) => {
+app.use((req, res, next) => {
   if (req.path === '/api/upload') {
     next();
   } else {
@@ -94,7 +94,7 @@ app.use(lusca.xssProtection(true));
 app.use((req, res, next) => {
   res.locals.user = req.user;
   next();
-});*/
+});
 app.use((req, res, next) => {
   // After successful login, redirect back to /api, /contact or /
   if (/(api)|(contact)|(^\/$)/i.test(req.path)) {
