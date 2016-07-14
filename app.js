4cc970bf6697154af4d8aeb7a78e3abd5d757e7b
Sibusiso Masemola <sbulink@users.noreply.github.com>
2016-07-14 11:34:21 +0200

    commented out 2lines, fixing post https

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.use((req, res, next) => {
   }
 });
 //app.use(lusca.xframe('SAMEORIGIN'));
-app.use(lusca.xssProtection(true));
+//app.use(lusca.xssProtection(true));
 app.use((req, res, next) => {
   res.locals.user = req.user;
   next();
