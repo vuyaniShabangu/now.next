321bd5b73ca5ef1bd401bec782ec4ac8379ea850
vuyaniShabangu <vuyani.shabangu@gmail.com>
2016-09-03 16:53:17 +0200

    ficing undefined table error

--- public/js/main.js
+++ public/js/main.js
@@ -3,70 +3,6 @@ $(document).ready(function() {
   // Place JavaScript code here...
 
 
-
-    $.ajax({
-              async: false,
-              url : '/missionsemail',
-              type : 'GET',
-              success : function(data) {
-               // alert("My email address is: "+data);
-                currentUserEmail = data;
-                userCompletedmissions
-                .columns( 0 )
-                .search( data )
-                .draw();
-              },
-              error : function(request,error)
-              {
-              }
-        });
-
-//-------------------------------------------
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCZ1Y3-M9hpEbq0JR4F_Y7RuEHpt4bM9cI",
-    authDomain: "dronr-913d4.firebaseapp.com",
-    databaseURL: "https://dronr-913d4.firebaseio.com",
-    storageBucket: "dronr-913d4.appspot.com",
-  };
-  firebase.initializeApp(config);
-
-  
-  var uploader = document.getElementById('uploader');
-  var fileButton = document.getElementById('resUpload');
-
-  //Listen fo file selection
-  fileButton.addEventListener('change',  function(e){
-    //Get the file
-    var file = e.target.files[0];
-
-    //Create reference
-    var storageRef = firebase.storage().ref('missionReportFiles/' + file.name);
-
-    //Upload file
-    var task = storageRef.put(file);
-
-    //update progress
-    task.on('state_changed', 
-
-            function progress(snapshot){
-                var percentage = (snapshot.bytesTransferred / snapshot.totalBytes)*100;
-                uploader.value = percentage;
-            },
-
-            function error(err){
-
-            },
-
-            function complete(){
-
-            }
-
-      );
-  });
-
-
-  
 //FOR FINISHED MISSIONS
   var operatorCompletedmissions;
   //alert("ready to go! ");
@@ -145,6 +81,70 @@ userCompletedmissions
                 .columns( 7 )
                 .search( 'completed' )
                 .draw();
+
+    $.ajax({
+              async: false,
+              url : '/missionsemail',
+              type : 'GET',
+              success : function(data) {
+               // alert("My email address is: "+data);
+                currentUserEmail = data;
+                userCompletedmissions
+                .columns( 0 )
+                .search( data )
+                .draw();
+              },
+              error : function(request,error)
+              {
+              }
+        });
+
+//-------------------------------------------
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCZ1Y3-M9hpEbq0JR4F_Y7RuEHpt4bM9cI",
+    authDomain: "dronr-913d4.firebaseapp.com",
+    databaseURL: "https://dronr-913d4.firebaseio.com",
+    storageBucket: "dronr-913d4.appspot.com",
+  };
+  firebase.initializeApp(config);
+
+  
+  var uploader = document.getElementById('uploader');
+  var fileButton = document.getElementById('resUpload');
+
+  //Listen fo file selection
+  fileButton.addEventListener('change',  function(e){
+    //Get the file
+    var file = e.target.files[0];
+
+    //Create reference
+    var storageRef = firebase.storage().ref('missionReportFiles/' + file.name);
+
+    //Upload file
+    var task = storageRef.put(file);
+
+    //update progress
+    task.on('state_changed', 
+
+            function progress(snapshot){
+                var percentage = (snapshot.bytesTransferred / snapshot.totalBytes)*100;
+                uploader.value = percentage;
+            },
+
+            function error(err){
+
+            },
+
+            function complete(){
+
+            }
+
+      );
+  });
+
+
+  
 
   var currentUserEmail;
   var table;
