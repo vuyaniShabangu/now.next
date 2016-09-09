60417668b347b78433ce01cf722212ce8bc06377
MahobalaThosago <u13062060@tuks.co.za>
2016-09-09 11:56:54 +0200

    Map init

--- public/js/main.js
+++ public/js/main.js
@@ -19,11 +19,11 @@ $(document).ready(function() {
               },
               dataType:'json',
               success : function(data) {
-                initMap();
+                initPage();
               },
               error : function(request,error)
               {
-                initMap();
+                initPage();
               }
         });
   }
@@ -611,91 +611,6 @@ $('#operatordronesTable tbody').on( 'click', 'button#editButton', function () {
 
   //This is for the Google Map in the Create Mission Form:
 
-function initMap() {
-    document.getElementById('map').innerHTML = "dfsdfs";
-      var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: userLat, lng: userLong},
-        zoom: 10
-      });
-
-    // This event listener calls addMarker() when the map is clicked.
-      /*google.maps.event.addListener(map, 'click', function(event) {
-        console.log("point");
-        addMarker(event.latLng, map);
-      });*/
-
-        map.addListener('mousemove', function() {
-          // 3 seconds after the center of the map has changed, pan back to the
-          // marker.
-          //  alert("papapapa");
-        });
-
-      var drawingManager = new google.maps.drawing.DrawingManager({
-        drawingMode: google.maps.drawing.OverlayType.POLYLINE,
-        drawingControl: false,
-        circleOptions: {
-            fillColor: '#ffff00',
-            fillOpacity: 1,
-          strokeWeight: 5,
-            clickable: false,
-            editable: true,
-            zIndex: 1
-        }
-      });
-
-
-
-      /*google.maps.event.addListener(drawingManager, "overlaycomplete", function(event){
-               // overlayClickListener(event.overlay);
-               // $('#missiondesc').html(event.overlay.getPath().getArray());
-
-                console.log("done!");
-                console.log(event.overlay.getPath().getArray()[0].lat());
-                overlayPointsArray = event.overlay.getPath().getArray()
-
-          surveillanceRoute = Array();
-
-          for (var i = 0; i < overlayPointsArray.length; i++) {
-            surveillanceRoute[i] = Object();
-            surveillanceRoute[i].lat = overlayPointsArray[i].lat();
-            surveillanceRoute[i].lng = overlayPointsArray[i].lng();
-          }
-
-          console.log(surveillanceRoute);
-            });*/
-
-
-
-
-
-
-    drawingManager.setMap(map)
-  }
-
-  function addMarker(location, map) {
-      // Add the marker at the clicked location, and add the next-available label
-      // from the array of alphabetical characters.
-      var marker = new google.maps.Marker({
-        position: location,
-        map: map
-      });
-  }
-
-  function addLatLng(event) {
-    console.log("gf");
-  //var path = poly.getPath();
-
-  // Because path is an MVCArray, we can simply append a new coordinate
-  // and it will automatically appear.
-  //path.push(event.latLng);
-
-  // Add a new marker at the new plotted point on the polyline.
-  var marker = new google.maps.Marker({
-    position: event.latLng,
-    //title: '#' + path.getLength(),
-    map: map
-  });
-}
 
 
 function downloadInnerHtml(filename, elId, mimeType) {
@@ -739,7 +654,7 @@ $('#acceptedmissionsgrid tbody').on( 'click', 'button#downloadWP', function () {
 });
 
 
-
+function initPage(){
   $.getScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyAWiEnhMjv7lLDyaoiwIHwEVYoMRN4nYKY&libraries=drawing', function()
   {
                       //This is for the Google Map in the Create Mission Form:
@@ -846,7 +761,7 @@ $('#acceptedmissionsgrid tbody').on( 'click', 'button#downloadWP', function () {
                       });
                     }
 
-                    //  initMap();
+                      initMap();
   });
-
+}
 });
