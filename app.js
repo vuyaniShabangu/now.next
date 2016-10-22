5dc3c60fc35961d1e689c390d710d37f5579575c
Vuyani M. Shabangu <vuyani.shabangu@gmail.com>
2016-10-23 01:43:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -48,6 +48,12 @@ const passportConfig = require('./config/passport');
  */
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 /**
  * Connect to MongoDB.
  */
